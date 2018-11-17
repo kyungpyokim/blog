ceb1903fd2c1188e2a8f25c06742ae4c3f9c8a42
kyungpyokim <lammye@gmail.com>
2018-11-17 18:12:53 +0900

    Add mongoDB

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,94 @@
+let postId = 1;
+
+const posts = [
+  {
+    id: 1,
+    title: 'subject',
+    body: 'content'
+  }
+];
+
+const message = "doesn't exist post";
+
+exports.write = (ctx) => {
+  const { title, body } = ctx.request.body;
+
+  postId += 1;
+
+  const post = { id: postId, title, body };
+  posts.push(post);
+
+  ctx.body = post;
+};
+
+exports.list = (ctx) => {
+  ctx.body = posts;
+};
+
+exports.read = (ctx) => {
+  const { id } = ctx.params;
+  const post = posts.find(p => p.id.toString() === id);
+
+  if (!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message
+    };
+    return;
+  }
+  ctx.body = post;
+};
+
+exports.remove = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex(p => p.id.toString() === id);
+
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message
+    };
+    return;
+  }
+
+  posts.splice(index, 1);
+  ctx.status = 204;
+};
+
+exports.replace = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex(p => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message
+    };
+    return;
+  }
+
+  posts[index] = {
+    id,
+    ...ctx.request.body
+  };
+  ctx.body = posts[index];
+};
+
+exports.update = (ctx) => {
+  const { id } = ctx.params;
+  console.log(id);
+  const index = posts.findIndex(p => p.id.toString() === id);
+
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message
+    };
+    return;
+  }
+
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body
+  };
+  ctx.body = posts[index];
+};
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,11 +1,39 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+router.use('/api', api.routes());
 
-app.listen(4000, () => {
-  console.log('listening to port 4000');
+app.use(bodyParser());
+
+// router.get('/', (ctx) => {
+//   ctx.body = 'Home';
+// });
+
+// router.get('/about', (ctx) => {
+//   ctx.body = 'Introduce';
+// });
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log('listening to port', port);
 });
