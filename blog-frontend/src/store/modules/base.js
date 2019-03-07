c137fe6eefb34f42e51d31cd636f275aa3be13d8
kyungpyokim <lammye@gmail.com>
2019-03-07 22:12:23 +0900

    Fix typo and add page

--- blog-frontend/src/store/modules/base.js
+++ blog-frontend/src/store/modules/base.js
@@ -1,8 +1,8 @@
-import { createAction, handleAction } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
 
 import { Map } from "immutable";
 import { pender } from "redux-pender";
 
 const initialState = Map({});
 
-export default handleAction({}, initialState);
+export default handleActions({}, initialState);
