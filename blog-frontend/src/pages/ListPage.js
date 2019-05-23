91f8fb1c243c957dd844619d7c4c605236a0205c
KyungPyoKim <kyungpyokim@KyungPyoKimui-MacBook-Pro.local>
2019-05-23 22:34:05 +0900

    Add ListWrapper

--- blog-frontend/src/pages/ListPage.js
+++ blog-frontend/src/pages/ListPage.js
@@ -1,8 +1,13 @@
 import React from "react";
-import PageTemplate from "../components/common/PageTemplate";
+import PageTemplate from "components/common/PageTemplate";
+import ListWrapper from "components/list/ListWrapper";
 
 const ListPage = () => {
-  return <PageTemplate>List</PageTemplate>;
+  return (
+    <PageTemplate>
+      <ListWrapper>리스트</ListWrapper>
+    </PageTemplate>
+  );
 };
 
 export default ListPage;
