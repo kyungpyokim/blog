d841dc39222219473593f099b9cd4bb7f44ce8a9
KyungPyoKim <kyungpyokim@KyungPyoKimui-MacBook-Pro.local>
2019-05-28 22:43:13 +0900

    Add PostList

--- blog-frontend/src/pages/ListPage.js
+++ blog-frontend/src/pages/ListPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import PageTemplate from "components/common/PageTemplate";
 import ListWrapper from "components/list/ListWrapper";
+import PostList from "components/list/PostList";
 
 const ListPage = () => {
   return (
     <PageTemplate>
-      <ListWrapper>리스트</ListWrapper>
+      <ListWrapper>
+        <PostList />
+      </ListWrapper>
     </PageTemplate>
   );
 };
