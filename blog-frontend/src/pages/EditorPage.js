9092c87650644135cdbe8564d50dbf24c7198eb3
KyungPyoKim <kyungpyokim@KyungPyoKimui-MacBook-Pro.local>
2019-06-13 00:22:26 +0900

    Add editorTemplate

--- blog-frontend/src/pages/EditorPage.js
+++ blog-frontend/src/pages/EditorPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import EditorTemplate from 'components/editor/EditorTemplate';
 
 const EditorPage = () => {
-  return <div>EditorPage</div>;
+  return (
+    <EditorTemplate
+      header="헤더"
+      editor="에디터"
+      preview="프리뷰" />
+  )
 };
 
 export default EditorPage;
