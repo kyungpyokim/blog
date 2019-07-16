a2fc45715ca96dc8402ebeb15ece478fe8cf4b39
KyungPyoKim <kyungpyokim@KyungPyoKimui-MacBook-Pro.local>
2019-07-17 00:13:24 +0900

    Apply markdownRnder.scss

--- blog-frontend/src/components/common/MarkdownRender/index.js
+++ blog-frontend/src/components/common/MarkdownRender/index.js
@@ -4,6 +4,14 @@ import classNames from 'classnames/bind';
 
 import marked from 'marked';
 
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-okaidia.css';
+
+import 'prismjs/components/prism-bash.min.js';
+import 'prismjs/components/prism-javascript.min.js';
+import 'prismjs/components/prism-jsx.min.js';
+import 'prismjs/components/prism-css.min.js';
+
 const cx = classNames.bind(styles);
 
 class MarkdownRender extends Component {
@@ -40,6 +48,9 @@ class MarkdownRender extends Component {
   componentDidUpdate(prevProps, prevState) {
     if(prevProps.markdown !== this.props.markdown) {
       this.renderMarkdown();
+    }
+    if(prevState.html !== this.state.html) {
+      Prism.highlightAll();
     }
   }
 
