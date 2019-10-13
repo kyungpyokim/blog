da9ce30d2f13e49f9bd9bb918b41ae2ea28bccb5
kyungpyo.kim <kyungpyo.kim0@gmail.com>
2019-10-13 22:20:37 +0900

    Add Post and use it

--- blog-frontend/src/store/modules/post.js
+++ blog-frontend/src/store/modules/post.js
@@ -1,8 +1,24 @@
 import { createAction, handleActions } from "redux-actions";
 
-import { Map } from "immutable";
+import { Map, fromJS } from "immutable";
 import { pender } from "redux-pender";
 
-const initialState = Map({});
+import * as api from 'lib/api';
 
-export default handleActions({}, initialState);
+const GET_POST = 'post/GET_POST';
+
+export const getPost = createAction(GET_POST, api.getPost);
+
+const initialState = Map({
+  post: Map({}),
+});
+
+export default handleActions({
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const { data: post } = action.payload;
+      return state.set('post', fromJS(post));
+    }
+  })
+}, initialState);
